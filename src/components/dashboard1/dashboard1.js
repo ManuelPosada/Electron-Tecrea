2bcc4fde2bf9a4f14c2c038d0e8cedb0b534c568
unknown <manuel3811@hotmail.com>
2020-10-07 18:39:37 -0500

    Dashboard 1 main components ready

--- src/components/dashboard1/dashboard1.js
+++ src/components/dashboard1/dashboard1.js
@@ -11,7 +11,9 @@ const Analog1RangeMin =  document.getElementById("Analog1RangeMin").addEventList
     Analog1spanMin.innerText = event.target.value;
 });
 
-document.getElementById("AnalogSwitch1").addEventListener("change",(event) => {
+document.getElementById("AnalogSwitch1").addEventListener("change", (event) => {
+    console.log(event.target.value)
+    console.log(event.target.checked)
     if( !event.target.checked ){
         document.getElementById("Analog1RangeMax").disabled = true;
         document.getElementById("Analog1RangeMin").disabled = true;
@@ -45,6 +47,35 @@ document.getElementById("AnalogSwitch2").addEventListener("change",(event) => {
 })
 
 /**
+ *  Battery
+*/
+const BatterySpan = document.getElementById("BatterySpan");
+const BatteryRange =  document.getElementById("BatteryRange").addEventListener("input",(event) => {
+    BatterySpan.innerText = event.target.value;
+});
+
+document.getElementById("BatterySwitch").addEventListener("change",(event) => {
+    if( !event.target.checked ) {
+        document.getElementById("BatteryRange").disabled = true;
+    }else{
+        document.getElementById("BatteryRange").disabled = false;
+    }
+})
+
+/**
+ *  Digital Inputs Selector
+*/
+document.getElementById("DISwitch").addEventListener("change",(event) => {
+    if( !event.target.checked ) {
+        document.getElementById("DigitalIn1Select").disabled = true;
+        document.getElementById("DigitalIn2Select").disabled = true;
+    }else{
+        document.getElementById("DigitalIn1Select").disabled = false;
+        document.getElementById("DigitalIn2Select").disabled = false;
+    }
+})
+
+/**
  *  Humidity
 */
 const HumidityspanMax = document.getElementById("HumidityspanMax");
@@ -67,6 +98,50 @@ document.getElementById("HumiditySwitch").addEventListener("change",(event) => {
     }
 })
 
+/**
+ *  Temperature
+*/
+const TempspanMax = document.getElementById("TempSpanMax");
+const TempRangeMax =  document.getElementById("TempRangeMax").addEventListener("input",(event) => {
+    TempspanMax.innerText = event.target.value;
+});
+
+const TempspanMin = document.getElementById("TempSpanMin");
+const TempRangeMin =  document.getElementById("TempRangeMin").addEventListener("input",(event) => {
+    TempspanMin.innerText = event.target.value;
+});
+
+document.getElementById("TempSwitch").addEventListener("change",(event) => {
+    if( !event.target.checked ){
+        document.getElementById("TempRangeMax").disabled = true;
+        document.getElementById("TempRangeMin").disabled = true;
+    }else{
+        document.getElementById("TempRangeMax").disabled = false;
+        document.getElementById("TempRangeMin").disabled = false;
+    }
+})
+
+/**
+ *  Sigfox Zone Selector
+*/
+document.getElementById("ZoneSwitch").addEventListener("change",(event) => {
+    if( !event.target.checked ) {
+        document.getElementById("ZoneSelect").disabled = true;
+    }else{
+        document.getElementById("ZoneSelect").disabled = false;
+    }
+})
+
+/**
+ *  Led Color Selector
+*/
+document.getElementById("ColorSwitch").addEventListener("change",(event) => {
+    if( !event.target.checked ) {
+        document.getElementById("ColorSelect").disabled = true;
+    }else{
+        document.getElementById("ColorSelect").disabled = false;
+    }
+})
 
 document.getElementById("form-dashboard1").addEventListener("submit",(event) => {
     console.log(event.target);
