7a5a6c17a0637051bf59f789dbb987e629b3cda2
unknown <manuel3811@hotmail.com>
2020-10-20 09:15:39 -0500

    main size changes

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ let win;
 
 function createWindow () {
     win = new BrowserWindow({
-        width:  800,
-        height: 600,
+        width:  1300,
+        height: 800,
         webPreferences: {
             nodeIntegration: true,
             worldSafeExecuteJavaScript: true
